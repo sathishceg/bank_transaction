c6dee0865f1d9af16a04e316cb2e44c07b19377f
sathishceg <satbruceitan@gmail.com>
2018-10-04 19:54:25 +0530

    app setup

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const app = require("express")();
 const bodyParser = require('body-parser');
 const PORT = 3000;
+const routes = require('./lib/routes');
+const db = require('./lib/db');
 
 app.use(bodyParser.json());
-app.listen(PORT, ()=> {
-  console.log("Server Started");
+app.use('/',routes);
+
+function startServer(){
+  return new Promise((resolve, reject) => {
+    app.listen(PORT, (err)=> {
+      if(err){
+        return reject(err);
+      }
+      return resolve();
+    })
+  });
+}
+
+async function setup(){
+  await db.connect();
+  await startServer();
+}
+
+setup().then(() => {
+  console.log("Server started");
+}).catch((err) => {
+  console.log("Server starting failed", err.stack);
 })
