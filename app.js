4c92d7cb54ee64aed31b5ee3d83a965ada2ddfd9
sathishceg <satbruceitan@gmail.com>
2018-10-10 14:38:58 +0530

    added comments and validation

--- app.js
+++ app.js
@@ -19,9 +19,11 @@ function startServer(){
   });
 }
 
+// This will do setup before starting server
 async function setup(){
   const arg = process.argv.slice(2)[0];
   await db.connect();
+  // Will create tables and create sample record if bootstrap option is passed.
   if(arg && arg == BOOTSTRAP){
     await db.createBalanceTable();
     await db.createTransactionsTable();
