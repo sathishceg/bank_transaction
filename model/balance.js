e3cd2ee8d77d63b1213d69f5366bbc217d88c04f
sathishceg <satbruceitan@gmail.com>
2018-10-10 09:18:47 +0530

    updating balance bug fix

--- model/balance.js
+++ model/balance.js
@@ -8,10 +8,25 @@ const ERROR_TYPE = "BALANCE_UPDATE_FAILED";
 const UPDATE_ERROR_MSG = "Error while updating balance, please try after sometime";
 const INTERNAL_SERVER_ERROR_CODE = 500;
 
-function insertOrUpdate(amount, acc_num){
+function insertOrUpdateToAccount(amount, acc_num){
   return new Promise((resolve, reject) => {
-    const query = `INSERT INTO ${TABLE_NAME} SET ${FIELDS.acc_no} = ?, ${FIELDS.balance} = ? ON DUPLICATE KEY UPDATE balance = balance + ?`;
+    const query = `INSERT INTO ${TABLE_NAME} SET ${FIELDS.acc_no} = ?, ${FIELDS.balance} = ? ON DUPLICATE KEY UPDATE ${FIELDS.balance} = ${FIELDS.balance} + ?`;
     db.connection.query(query, [acc_num, amount, amount], (error, results, fields) => {
+      if(error){
+        connection.rollback(function() {
+          update_error_type(error);
+          reject(error);
+        });
+      }
+      resolve();
+    });
+  });
+}
+
+function updateFromAccount(amount, acc_num){
+  return new Promise((resolve, reject) => {
+    const query = `UPDATE ${TABLE_NAME} set ${FIELDS.balance} = ${FIELDS.balance} - ? WHERE ${FIELDS.acc_no} = ?`;
+    db.connection.query(query, [amount, acc_num], (error, results, fields) => {
       if(error){
         connection.rollback(function() {
           update_error_type(error);
@@ -55,9 +70,10 @@ function update_error_type(err){
 
 class Balance{
 
-  async updateBalance(amount, acc_num){
+  async updateBalance(amount, to_acc_num, from_acc_num){
     await beginTransaction();
-    await insertOrUpdate(amount, acc_num);
+    await insertOrUpdateToAccount(amount, to_acc_num);
+    await updateFromAccount(amount, from_acc_num);
     await commitTransaction();
   }
 
